0ff1a75945a1282ad8f59fd74de9767b2354f08e
ZhangAoCanada <zhangaocanada@gmail.com>
2019-11-16 15:14:22 -0500

    update

---
+++ attackers/adversarial_samples.py
@@ -0,0 +1,108 @@
+##### set specific gpu #####
+import os
+# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+############################
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+from tensorflow.keras.models import load_model
+
+from art import DATA_PATH
+from art.utils import load_dataset, get_file
+from art.classifiers import KerasClassifier
+from art.attacks import FastGradientMethod, ElasticNet
+
+import numpy as np
+import matplotlib
+matplotlib.use("tkagg")
+import matplotlib.pyplot as plt
+import time
+
+def GetAdvAccuracy(keras_model, data_true, data_adv, y_true):
+    """
+    Function:
+        Get accuracy loss, perturbation and time duration on the specific 
+        testing data (test set or adversarial set).
+    """
+    _, true_acc = keras_model.evaluate(data_true, y_true)
+    _, adv_acc = keras_model.evaluate(data_adv, y_true)
+    confidence_diff = true_acc - adv_acc
+    perturbation = np.mean(np.abs(data_true - data_adv))
+    print('Average Confidence lost: {:4.2f}%'.format(confidence_diff * 100))
+    print('Average Image perturbation: {:4.2f}'.format(perturbation))
+    return confidence_diff, perturbation
+
+def GetCifar10WithModel():
+    """
+    Function:
+        Load cifar-10 dataset and load a pre-trained cifar10 model.
+    """
+    (x_train, y_train), (x_test, y_test), min_, max_ = load_dataset('cifar10')
+    num_samples_train = 100
+    num_samples_test = 100
+    x_train = x_train[0:num_samples_train]
+    y_train = y_train[0:num_samples_train]
+    x_test = x_test[0:num_samples_test]
+    y_test = y_test[0:num_samples_test]
+
+    class_descr = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+    path = get_file('cifar_resnet.h5',extract=False, path=DATA_PATH,
+                    url='https://www.dropbox.com/s/ta75pl4krya5djj/cifar_resnet.h5?dl=1')
+    classifier_model = load_model(path)
+
+    classifier_model.summary()
+    return x_train, y_train, x_test, y_test, classifier_model, min_, max_
+
+def GetAttackers(classifier, x_test, attacker_name):
+    """
+    Function:
+        Load classifier and generate adversarial samples
+    """
+    t_start = time.time()
+    if attacker_name == "FGSM":
+        attacker = FastGradientMethod(classifier=classifier, eps=0.05)
+    elif attacker_name == "Elastic":
+        attacker = ElasticNet(classifier=classifier, binary_search_steps=3, max_iter=10)
+    else:
+        raise ValueError("Please get the right attacker's name for the input.")
+    test_adv = attacker.generate(x_test)
+    dt = time.time() - t_start
+    return test_adv, dt
+
+def debug():
+    """
+    Function:
+        For debugging.
+    For attacker_name:
+        "FGSM"
+        "Elastic"
+    add your attacker's name here.
+    """
+    x_train, y_train, x_test, y_test, model, min_, max_ = GetCifar10WithModel()
+    x_test_example = x_test[:2]
+    y_test_example = y_test[:2]
+
+    classifier = KerasClassifier(clip_values=(min_, max_), model=model, use_logits=False, 
+                                preprocessing=(0.5, 1))
+    
+    x_adv_fgsm, dt_fgsm = GetAttackers(classifier, x_test_example, "FGSM")
+    x_adv_elastic, dt_elastic = GetAttackers(classifier, x_test_example, "Elastic")
+    print("Time duration for FGSM: \t", dt_fgsm)
+    print("Time duration for Elastic: \t", dt_elastic)
+    print("-------------------------------------------")
+
+    conf_l_fgsm, perturb_fgsm = GetAdvAccuracy(model, x_test_example, x_adv_fgsm, y_test_example)
+    print("-------------------------------------------")
+    conf_l_elast, perturb_elast = GetAdvAccuracy(model, x_test_example, x_adv_elastic, y_test_example)
+
+if __name__ == "__main__":
+    debug()
