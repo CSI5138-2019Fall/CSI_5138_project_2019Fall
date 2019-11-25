3be6d4610ed52b6bee3a316324b1bc2ef085673f
Ao Zhang <zhangaocanada@gmail.com>
2019-11-24 22:00:36 -0500

    whatever

--- reinforcement_black_box/run.py
+++ reinforcement_black_box/run.py
@@ -10,9 +10,9 @@ import tensorflow as tf
 import tensorflow.keras as keras
 # tf.compat.v1.disable_eager_execution()
 # ##### gpu memory management #####
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# physical_devices = tf.config.experimental.list_physical_devices('GPU')
+# assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 from tensorflow.keras.models import load_model
 
