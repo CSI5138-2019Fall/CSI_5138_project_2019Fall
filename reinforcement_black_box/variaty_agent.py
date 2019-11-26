9b7f0a41e63c030896fe4eb116e3495adfe654a0
Lingfeng <zlf465074419@gmail.com>
2019-11-26 13:02:25 -0500

    update

---
+++ reinforcement_black_box/variaty_agent.py
@@ -0,0 +1,9 @@
+##### set specific gpu #####
+import os
+# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import pickle
