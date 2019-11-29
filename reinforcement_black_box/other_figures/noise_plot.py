19069d4d02e08de46cd3e1cd81906f8f4f80c170
ZhangAoCanada <zhangaocanada@gmail.com>
2019-11-29 14:39:29 -0500

    update some figures

---
+++ reinforcement_black_box/other_figures/noise_plot.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+import math
+import seaborn
+
+mu = 0
+sigma = 0.5
+x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
+x_new = x[x >= 0]
+x_new = x_new[x_new <= 1]
+x_zeros = np.zeros(len(x_new))
+
+fig = plt.figure(figsize = (10, 6))
+ax = fig.add_subplot(111)
+
+ax.plot(x, stats.norm.pdf(x, mu, sigma), 'black')
+ax.axvline(x=0.0, ls='-.', color='black')
+ax.axvline(x=1.0, ls='-.', color='black')
+ax.grid(linestyle='--', linewidth=0.5, alpha=0.8)
+ax.text(-1.4, .75, r'$\mu=0.,\ \sigma=0.5*epsilon$')
+ax.fill_between(x_new, stats.norm.pdf(x_new, mu, sigma), alpha=0., hatch='/')
+ax.set_xlim([-1.5, 1.5])
+ax.set_ylim([0, 0.85])
+ax.set_xlabel('deviation (* epsilon)')
+ax.set_ylabel('probability')
+
+# ax.spines['left'].set_position('zero')
+# ax.spines['right'].set_color('none')
+# ax.spines['bottom'].set_position('zero')
+# ax.spines['top'].set_color('none')
+
+# # remove the ticks from the top and right edges
+# ax.xaxis.set_ticks_position('bottom')
+# ax.yaxis.set_ticks_position('none')
+
+# seaborn.despine(ax=ax, offset=0)
+plt.savefig("./noise.png")
+plt.show()
